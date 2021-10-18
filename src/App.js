b3724a4ac74a69953e10a88e1c205d4dec01070b
dowap2 <dowapdowari@gmail.com>
2021-10-18 20:41:54 +0900

    add Crawling part to search my character profile

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import cheerio from "cheerio";
+
+async function getHTML() {
+  const characterName = "스태프로꽝";
+  try {
+    return await axios.get(
+      `https://lostark.game.onstove.com/Profile/Character/${characterName}`
+    );
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getHTML()
+  .then(html => {
+    let titleList = [];
+    const $ = cheerio.load(html.data);
+    const bodyList = $("div.level-info2__expedition");
+
+    bodyList.each(function(i, elem) {
+      titleList[i] = {
+        title: $(this)
+          .find("span")
+          .text()
+      };
+    });
+    return titleList;
+  })
+  .then(res => console.log(res));
 
 function App() {
   return (
