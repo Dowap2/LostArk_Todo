6b13ade1698819748074be1cca00d5c5c68440df
dowap2 <dowapdowari@gmail.com>
2021-10-26 19:14:38 +0900

    add router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { Route } from "react-router-dom";
 import SearchCharacter from "./search/SearchCharacter";
 
 const lvDefault = ["도전어비스", "도전가디언"];
@@ -9,54 +9,9 @@ const lv1415 = ["발탄"];
 const lv1430 = ["아브렐슈드데자뷰", "비아키스"];
 
 function App() {
-  const [level, setLevel] = useState(0);
-  const [character, setCharacter] = useState("");
-  const [characterName, setCharacterName] = useState("");
-  const [content, setContent] = useState([]);
-  const [contentToCheckBox, setContentToCheckBox] = useState([]);
-
-  //   const setContentList = level => {
-  //     if (level >= 1325) {
-  //       setContent(content.concat(lvDefault, lv1325));
-  //     }
-  //     if (level >= 1370) {
-  //       setContent(content.concat(lvDefault, lv1325, lv1370));
-  //     }
-  //     if (level >= 1385) {
-  //       setContent(content.concat(lvDefault, lv1325, lv1370, lv1385));
-  //     }
-  //     if (level >= 1415) {
-  //       setContent(content.concat(lvDefault, lv1325, lv1370, lv1385, lv1415));
-  //     }
-  //     if (level >= 1430) {
-  //       setContent(
-  //         content.concat(lvDefault, lv1325, lv1370, lv1385, lv1415, lv1430)
-  //       );
-  //     }
-  //   };
-  // }, [characterName]);
-
-  // useEffect(() => {
-  //   const html = content.map(content => {
-  //     return (
-  //       <div>
-  //         <label>{content}</label>
-  //         <input type="checkbox"></input>
-  //       </div>
-  //     );
-  //   });
-  //   setContentToCheckBox(html);
-  // }, [content]);
-
   return (
     <div className="App">
-      <SearchCharacter></SearchCharacter>
-      {/* <input type="text" onChange={e => setCharacter(e.target.value)}></input>
-      <button onClick={e => setCharacterName(character)}></button>
-      Lv.{level}
-      <button onClick={e => console.log(content)}></button>
-      <button onClick={e => setContent([])}></button>
-      <div>{contentToCheckBox}</div> */}
+      <Route path="/search" component={SearchCharacter} />
     </div>
   );
 }
