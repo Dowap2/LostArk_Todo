b4e26abf7a2caabf6c3d2cb16125aa70951bd15a
dowap2 <dowapdowari@gmail.com>
2021-10-29 21:31:31 +0900

    add content data in search page

--- src/search/CharacterView.jsx
+++ src/search/CharacterView.jsx
@@ -46,6 +46,7 @@ const DetailInfoBox = styled.div`
 `;
 const DetailInfoTag = styled.div`
   width: 100px;
+  height: 20px;
   padding: 10px;
   background: #8697a6;
   color: #ffffff;
@@ -76,73 +77,98 @@ const CharacterView = props => {
   const [content, setContent] = useState();
 
   useEffect(() => {
-    switch (data[3]) {
-      case "디스트로이어":
-        setImgSrc(destroyer);
-        break;
-      case "워로드":
-        setImgSrc(warlord);
-        break;
-      case "버서커":
-        setImgSrc(berserker);
-        break;
-      case "홀리나이트":
-        setImgSrc(holyknight);
-        break;
-      case "배틀마스터":
-        setImgSrc(battlemaster);
-        break;
-      case "인파이터":
-        setImgSrc(infighter);
-        break;
-      case "기공사":
-        setImgSrc(soulmaster);
-        break;
-      case "창술사":
-        setImgSrc(lancemaster);
-        break;
-      case "스트라이커":
-        setImgSrc(striker);
-        break;
-      case "데빌헌터":
-        setImgSrc(striker);
-        break;
-      case "블래스터":
-        setImgSrc(blaster);
-        break;
-      case "호크아이":
-        setImgSrc(striker);
-        break;
-      case "스카우터":
-        setImgSrc(scouter);
-        break;
-      case "건슬링어":
-        setImgSrc(gunslinger);
-        break;
-      case "바드":
-        setImgSrc(bard);
-        break;
-      case "서머너":
-        setImgSrc(summoner);
-        break;
-      case "아르카나":
-        setImgSrc(arcana);
-        break;
-      case "소서리스":
-        setImgSrc(sorceress);
-        break;
-      case "블레이드":
-        setImgSrc(blade);
-        break;
-      case "데모닉":
-        setImgSrc(demonic);
-        break;
-      case "리퍼":
-        setImgSrc(reaper);
-        break;
-      default:
-        console.log("error");
+    if (data[0] !== undefined) {
+      switch (data[3]) {
+        case "디스트로이어":
+          setImgSrc(destroyer);
+          break;
+        case "워로드":
+          setImgSrc(warlord);
+          break;
+        case "버서커":
+          setImgSrc(berserker);
+          break;
+        case "홀리나이트":
+          setImgSrc(holyknight);
+          break;
+        case "배틀마스터":
+          setImgSrc(battlemaster);
+          break;
+        case "인파이터":
+          setImgSrc(infighter);
+          break;
+        case "기공사":
+          setImgSrc(soulmaster);
+          break;
+        case "창술사":
+          setImgSrc(lancemaster);
+          break;
+        case "스트라이커":
+          setImgSrc(striker);
+          break;
+        case "데빌헌터":
+          setImgSrc(striker);
+          break;
+        case "블래스터":
+          setImgSrc(blaster);
+          break;
+        case "호크아이":
+          setImgSrc(striker);
+          break;
+        case "스카우터":
+          setImgSrc(scouter);
+          break;
+        case "건슬링어":
+          setImgSrc(gunslinger);
+          break;
+        case "바드":
+          setImgSrc(bard);
+          break;
+        case "서머너":
+          setImgSrc(summoner);
+          break;
+        case "아르카나":
+          setImgSrc(arcana);
+          break;
+        case "소서리스":
+          setImgSrc(sorceress);
+          break;
+        case "블레이드":
+          setImgSrc(blade);
+          break;
+        case "데모닉":
+          setImgSrc(demonic);
+          break;
+        case "리퍼":
+          setImgSrc(reaper);
+          break;
+        default:
+          console.log("error");
+      }
+      const level = Number(data[6].replace(",", ""));
+      if (1370 > level && level >= 1325) {
+        setContent(lv1325);
+      } else if (1385 > level && level >= 1370) {
+        setContent(lv1370);
+      } else if (1400 > level && level >= 1385) {
+        setContent(lv1385);
+      } else if (1415 > level && level >= 1400) {
+        setContent(lv1400);
+      } else if (1430 > level && level >= 1415) {
+        setContent(lv1415);
+      } else if (1445 > level && level >= 1430) {
+        setContent(lv1430);
+      } else if (level >= 1445) {
+        setContent(lv1445);
+      } else {
+        setContent(lvDefault);
+      }
     }
+    setContent(
+      content.map(content => {
+        return <div>{content}</div>;
+      })
+    );
   }, [data]);
 
   return data[0] !== undefined ? (
@@ -179,6 +205,10 @@ const CharacterView = props => {
         <DetailInfoBox>
           <DetailInfoTag>영지</DetailInfoTag>
           <DetailInfoText>{data[9]}</DetailInfoText>
+        </DetailInfoBox>
+        <DetailInfoBox>
+          <DetailInfoTag>콘텐츠</DetailInfoTag>
+          <DetailInfoText>{content}</DetailInfoText>
         </DetailInfoBox>
         <EnrollmentButton>등록</EnrollmentButton>
       </DetailInfoComponent>
